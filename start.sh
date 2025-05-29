90499620e329f7cd7cfd601db7a618cc88ddcf81
Py'tor <gmalyktoheed@gmail.com>
2025-05-29 14:53:13 +0500

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export STREAMLIT_SERVER_PORT=${PORT:-8080}
-export STREAMLIT_SERVER_ADDRESS=0.0.0.0
-export STREAMLIT_SERVER_HEADLESS=true
-export STREAMLIT_SERVER_ENABLE_CORS=false
-export STREAMLIT_SERVER_ENABLE_XSRF_PROTECTION=false
-
-streamlit run multilingual_voice_tutor_enhanced.py
