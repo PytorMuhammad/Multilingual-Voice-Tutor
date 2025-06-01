4f19bd2687de8c6f7d026f8de35f2e259c132e96
Py'tor <gmalyktoheed@gmail.com>
2025-06-01 18:47:45 +0500

    Create __init__.py

---
+++ components/audio_recorder/__init__.py
@@ -0,0 +1,20 @@
+import os
+import streamlit.components.v1 as components
+
+# Create the component
+_RELEASE = True
+
+if not _RELEASE:
+    _component_func = components.declare_component(
+        "audio_recorder",
+        url="http://localhost:3001",
+    )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _component_func = components.declare_component("audio_recorder", path=build_dir)
+
+def audio_recorder(key=None):
+    """Audio recorder component"""
+    component_value = _component_func(key=key, default=None)
+    return component_value
